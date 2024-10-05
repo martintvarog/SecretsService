07a3add183393271a0447e1f795dd94ea994f1ee
Martin Tvarog <martin.tvarog@gmail.com>
2024-10-05 18:29:34 +0200

    db updates

--- SecretsService.Model/Context/SecretsDbContext.cs
+++ SecretsService.Model/Context/SecretsDbContext.cs
@@ -4,7 +4,16 @@ namespace SecretsService.DataAccess.Context;
 
 public class SecretsDbContext : DbContext
 {
-    public SecretsDbContext(DbContextOptions<SecretsDbContext> options) : base(options) { }
+    public SecretsDbContext(DbContextOptions<SecretsDbContext> options) : base(options)
+    {
+    }
 
     public DbSet<Secret> Secrets { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Secret>()
+            .HasIndex(s => s.Name)
+            .IsUnique();
+    }
 }
--- SecretsService.Model/Secret.cs
+++ SecretsService.Model/Secret.cs
@@ -3,8 +3,8 @@ namespace SecretsService.DataAccess;
 public class Secret
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string Value { get; set; }
+    public string Name { get; set; } = null!;
+    public string Value { get; set; } = null!;
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 }
