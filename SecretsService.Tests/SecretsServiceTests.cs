05e9526b0079705e943d43d2bf43ffe66b444db2
Martin Tvarog <martin.tvarog@gmail.com>
2024-10-05 20:17:39 +0200

    add tests

---
+++ SecretsService.Tests/SecretsServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using NSubstitute;
+using SecretsService.DataAccess.Context;
+using SecretsService.Service.Contracts;
+
+namespace SecretsService.Tests;
+
+public class SecretsServiceTests
+{
+    private ISecretsService InitializeSecretsService()
+    {
+        return new Service.Services.SecretsService(Substitute.For<SecretsDbContext>(),
+            Substitute.For<IDataProtectionProvider>());
+    }
+}
