1186f1b5129e223724bd0a54b00c9654ebd098a1
Martin Tvarog <martin.tvarog@gmail.com>
2024-10-05 20:51:12 +0200

    inmemory

---
+++ SecretsService.Tests/InMemoryDb/SecretsInMemoryDb.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using SecretsService.Model.Context;
+
+namespace SecretsService.Tests.InMemoryDb;
+
+public class SecretsInMemoryDb
+{
+    public SecretsDbContext CreateDbContext()
+    {
+        var options = new DbContextOptionsBuilder<SecretsDbContext>()
+            .UseInMemoryDatabase(databaseName: "SecretsTestDb")
+            .Options;
+
+        return new SecretsDbContext(options);
+    }
+    
+}
