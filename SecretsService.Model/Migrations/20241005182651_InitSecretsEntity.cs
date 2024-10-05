5a2ed5c27a0b3c67545e2dd05cff0abf784123a2
Martin Tvarog <martin.tvarog@gmail.com>
2024-10-05 20:27:27 +0200

    migrations

---
+++ SecretsService.Model/Migrations/20241005182651_InitSecretsEntity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SecretsService.Model.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitSecretsEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Secrets",
+                columns: table => new
+                {
+                    SecretId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Secrets", x => x.SecretId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Secrets_Name",
+                table: "Secrets",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Secrets");
+        }
+    }
+}
